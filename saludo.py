4d021a0ef3387827babafad4b3d4736bb1f134ad
Luis_ <1737895@alu.murciaeduca.es>
2025-11-09 15:46:28 +0100

    Saludo básico personalizado con input

--- saludo.py
+++ saludo.py
@@ -1,5 +1,8 @@
 # saludo.py
-# Versión 2: Saludo personalizado con input
+# Versión 3: Cálculo de año de nacimiento
 
 nombre = input("¿Cómo te llamas? ")
 print(f"Hola, {nombre}!")
+
+edad = int(input("¿Qué edad tienes? "))
+print(f"Naciste en el año {2025 - edad}")
